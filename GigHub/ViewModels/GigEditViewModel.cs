93ff9f7bedbc4cd2c624e6d17a8e60ebb3ab85d7
fabrizioc <fabrizioc@silversea.com>
2018-03-09 16:59:50 +0100

    Add project files.

---
+++ GigHub/ViewModels/GigEditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GigHub.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.ViewModels
+{
+    public class GigEditViewModel : GigFormViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+    }
+}
---
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,42 @@
+﻿using GigHub.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
+        public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
+        public string Time { get; set; }
+
+        [Required]
+        public byte Genre { get; set; }
+
+        public IEnumerable<Genre> Genres { get; set; }
+
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+        public string Heading { get; set; }
+        public string Action {
+                    get
+                    {
+                        return (Id != 0) ? "Edit" : "Create";
+                            
+                    }
+                }
+                
+
+    }
+}
